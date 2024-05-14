7899ad336df2d644fcd72d8d30ebd177a972c13d
Caglayan Caliskan <caliskancaglayan@gmail.com>
2024-05-14 11:36:24 +0300

    Menu interactions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
     private int score;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+
+    private void Awake()
+    {
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+        gameOver.SetActive(false);
+        playButton.SetActive(false);
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
 
     public void IncreaseScore()
     {
         score++;
         Debug.Log("" + score);
+        scoreText.text = score.ToString();
     }
 
     public void GameOver()
     {
         Debug.Log("Game OVER");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        Pause();
     }
 }
